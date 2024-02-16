8319b155d3024ae7fcfe99b50bd71893b4c7a922
krunalbamrolia <krunal.bamroliya@gmail.com>
2024-02-17 00:03:24 +0530

    explorer porject

---
+++ explorer/src/components/Poster.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const poster = () => {
+  return (
+    <section class="showcase-area" id="showcase">
+    <div class="showcase-container">
+      <h1 class="main-title" id="home">Eat Right Food</h1>
+      <p>Eat Healty, it is good for our health.</p>
+      <a href="#food-menu" class="btn btn-primary">Menu</a>
+    </div>
+  </section>
+  )
+}
+
+export default poster
